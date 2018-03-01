b25788e9884fe59823a2d7d6e66a30f238d25dd1
Olga <kseveru@yandex.ru>
2018-03-01 23:54:32 +0300

    резиновый в горизонтальном положении

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,19 @@
 var navMain = document.querySelector(".site-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
-  navMain.classList.remove("nojs");
+navMain.classList.remove("nojs");
 
-  navToggle.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    navToggle.classList.toggle("main-nav__toggle--opened");
-    navMain.classList.toggle("site-nav--opened");
-  });
+navToggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  navToggle.classList.toggle("main-nav__toggle--opened");
+  navMain.classList.toggle("site-nav--opened");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (navToggle.classList.contains("main-nav__toggle--opened")) {
+      navToggle.classList.remove("main-nav__toggle--opened");
+      navMain.classList.remove("site-nav--opened");
+    }
+  }
+});
